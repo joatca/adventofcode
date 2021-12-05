e420a44d32348abfacd59c951e4d481cd8bb0c92
Fraser McCrossan <fraser@mccrossan.ca>
2021-12-05 14:55:52 -0500

    problem statement says we don't need to worry about anything other than perfect horizontals, verticals or diagonals

--- 5b.cr
+++ 5b.cr
@@ -6,11 +6,9 @@ STDIN.each_line(chomp: true) do |line|
   if line =~ /^(\d+),(\d+)\s*->\s*(\d+),(\d+)$/
     x1, y1, x2, y2 = $1.to_i, $2.to_i, $3.to_i, $4.to_i
     xinc, yinc, xmag, ymag = (x2 - x1).sign, (y2 - y1).sign, (x2 - x1).abs, (y2 - y1).abs
-    if xinc == 0 || yinc == 0 || xmag == ymag # ensure we only handle horizontals, verticals or diagonals
-      pixels = { xmag, ymag }.max + 1 # how many pixels we draw to complete the line
-      pixels.times do |pixel|
-        grid[{x1 + pixel * xinc, y1 + pixel * yinc}] += 1
-      end
+    pixels = { xmag, ymag }.max + 1 # how many pixels we draw to complete the line
+    pixels.times do |pixel|
+      grid[{x1 + pixel * xinc, y1 + pixel * yinc}] += 1
     end
   end
 end
