30dbd36d2857e8abbecfbfdd9e9ceb6e7049f423
Fraser McCrossan <fraser@mccrossan.ca>
2021-12-05 09:15:55 -0500

    much simpler and more general version of part 2 that turns out to be applicable to part 1 with one simple conditional change

--- 5b.cr
+++ 5b.cr
@@ -3,22 +3,11 @@ grid = Hash(Tuple(Int32, Int32), Int32).new(default_value: 0)
 STDIN.each_line(chomp: true) do |line|
   if line =~ /^(\d+),(\d+)\s*->\s*(\d+),(\d+)$/
     x1, y1, x2, y2 = $1.to_i, $2.to_i, $3.to_i, $4.to_i
-    if (x1 == x2) || (y1 == y2) # handle horizontal/vertical lines
-      x1, x2 = x2, x1 if x2 < x1 # flip x's to be increasing
-      y1, y2 = y2, y1 if y2 < y1 # and y's
-      # because we are guaranteed that either x's or y's will be constant we can pretend they form a rectangle
-      (x1..x2).each do |x|
-        (y1..y2).each do |y|
-          grid[{x, y}] += 1
-        end
-      end
-    elsif (x1-x2).abs == (y1-y2).abs # handle diagonal lines
-      x1, y1, x2, y2 = x2, y2, x1, y1 if x2 < x1 # ensure x1 is less than x2
-      ydiff = (y2 - y1) // (x2 - x1) # so this gives the y direction of the line as x increases
-      y = y1
-      (x1..x2).each do |x|
-        grid[{x, y}] += 1
-        y += ydiff
+    xinc, yinc, xmag, ymag = (x2 - x1).sign, (y2 - y1).sign, (x2 - x1).abs, (y2 - y1).abs
+    if xinc == 0 || yinc == 0 || xmag == ymag # ensure we only handle horizontals, verticals or diagonals
+      pixels = { xmag, ymag }.max + 1 # how many pixels we draw to complete the line
+      pixels.times do |pixel|
+        grid[{x1 + pixel * xinc, y1 + pixel * yinc}] += 1
       end
     end
   end
