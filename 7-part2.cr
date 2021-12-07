abd7c5913cc5dfe4ef7190c33bb7a1b01a268983
Fraser McCrossan <fraser@mccrossan.ca>
2021-12-07 16:59:45 -0500

    rename dist to distance to be clearer

--- 7-part2.cr
+++ 7-part2.cr
@@ -1,5 +1,5 @@
 STDIN.gets.try do |input|
   positions = input.split(',').map(&.to_i)
   minpos, maxpos = positions.minmax # the best horizontal position must be in this range
-  puts (minpos..maxpos).min_of { |pos| positions.map { |crab| (pos - crab).abs.try { |diff| diff * (diff + 1) // 2 } }.sum }
+  puts (minpos..maxpos).min_of { |pos| positions.map { |crab| (pos - crab).abs.try { |distance| distance * (distance + 1) // 2 } }.sum }
 end
