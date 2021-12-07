f05754ab88714590ef1bd53f2a74235e7e467e43
Fraser McCrossan <fraser@mccrossan.ca>
2021-12-07 16:57:48 -0500

    two insane code-reduction changes: factor out general arithmetic sequence sum function and use the specific version for this problem (i.e. a=1, d=1); use #try to avoid a temporary assignment inside the map block

--- 7-part2.cr
+++ 7-part2.cr
@@ -1,10 +1,5 @@
-# sum of an arithmetic sequence; a = first term, d = common difference, n = number of terms
-def arith(a : Int32, d : Int32, n : Int32)
-  n * (2*a + (n-1) * d) // 2
-end
-
 STDIN.gets.try do |input|
   positions = input.split(',').map(&.to_i)
   minpos, maxpos = positions.minmax # the best horizontal position must be in this range
-  puts (minpos..maxpos).min_of { |pos| positions.map { |crab| arith(1, 1, (pos-crab).abs) }.sum }
+  puts (minpos..maxpos).min_of { |pos| positions.map { |crab| (pos - crab).abs.try { |diff| diff * (diff + 1) // 2 } }.sum }
 end
