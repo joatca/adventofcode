ef680ad039feb0c5396f561082ac407b7d657bcd
Fraser McCrossan <fraser@mccrossan.ca>
2021-12-06 20:05:12 -0500

    employ a dirty hack to shave off two code lines

--- 6.cr
+++ 6.cr
@@ -2,9 +2,7 @@ STDIN.gets.try do |fish_s|
   school = Array(Int64).new(size: 9, value: 0) # gives counts of fish with current timer 0 through 8
   fish_s.split(',').each { |fish| school[fish.to_i] += 1 }
   (ARGV[0]? || "80").to_i.times do # do 80 by default or whatever is the first argument
-    spawned = school.shift # now the number of fish who were at zero is in spawned and all the others decrement by one
-    school[6] += spawned # the spawning fish reset to timer 6
-    school << spawned # and spawn extra fish with timer 8
+    school.rotate![6] += school[8] # rotate 0 count to pos 8 (spawned) then add that to 6 (original fish)
   end
-  p school.sum
+  puts school.sum
 end
