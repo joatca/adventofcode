b289ffb1267f23aa55ecfb36abb0bbbf333c3025
Fraser McCrossan <fraser@mccrossan.ca>
2021-12-06 20:19:16 -0500

    shave another two code lines because why not?

--- 6.cr
+++ 6.cr
@@ -1,8 +1,7 @@
 STDIN.gets.try do |fish_s|
   school = Array(Int64).new(size: 9, value: 0) # gives counts of fish with current timer 0 through 8
   fish_s.split(',').each { |fish| school[fish.to_i] += 1 }
-  (ARGV[0]? || "80").to_i.times do # do 80 by default or whatever is the first argument
-    school.rotate![6] += school[8] # DIRTY HACK ALERT rotate 0 count to pos 8 (spawned) then add that to 6 (original fish)
-  end
+  # DIRTY HACK ALERT rotate 0 count to pos 8 (spawned) then add that to 6 (original fish)
+  (ARGV[0]? || "80").to_i.times { school.rotate![6] += school[8] }
   puts school.sum
 end
