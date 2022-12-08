3a81d38434881732b0bbf4a7cee4cad41500f67e
Fraser McCrossan <fraser@mccrossan.ca>
2022-12-08 17:39:43 -0500

    all-in-one part 1 and part 2, better version; still not happy

--- 2022/8.cr
+++ 2022/8.cr
@@ -3,27 +3,29 @@ trees = STDIN.each_line(chomp: true).map { |line| line.each_char.map { |ch| ch.o
 raise "empty grid" if trees.size == 0
 raise "ragged grid" if trees[1..].any? { |row| row.size != trees[0].size }
 
-visible = 0
-max_score = 0
-best_x = best_y = 0 # this initial value doesn't matter
-max_x, max_y = trees[0].size - 1, trees.size - 1
-# we don't need to check the edge trees - at least one direction will be zero so the product score will be zero
-(1..(trees.size-2)).each do |y|
-  (1..(trees[y].size-2)).each do |x|
+visible, max_scenic = 0, 0
+range_x, range_y = 0..(trees[0].size-1), 0..(trees.size-1)
+# we don't need to check the edge trees - at least one direction will be zero so the product will be zero
+(1..(range_y.end-1)).each do |y|
+  (1..(range_x.end-1)).each do |x|
     height = trees[y][x]
-    most_x = (x+1).upto(max_x).find(max_x) { |xx| trees[y][xx] >= height }
-    least_x = (x-1).downto(0).find(0) { |xx| trees[y][xx] >= height }
-    most_y = (y+1).upto(max_y).find(max_y) { |yy| trees[yy][x] >= height }
-    least_y = (y-1).downto(0).find(0) { |yy| trees[yy][x] >= height }
-    visible += 1 if
-      (most_x == max_x && trees[y][most_x] < height) ||
-      (least_x == 0 && trees[y][least_x] < height) ||
-      (most_y == max_y && trees[most_y][x] < height) ||
-      (least_y == 0 && trees[least_y][x] < height)
-    score = (most_x-x) * (x-least_x) * (most_y-y) * (y-least_y)
-    if score > max_score
-      max_score, best_x, best_y = score, x, y
+    is_visible, scenic = false, 1
+    [ {0,1}, {1,0}, {0,-1}, {-1,0} ].each do |mx, my|
+      direction_visible, direction_scenic = true, 0
+      ix, iy = x+mx, y+my
+      while range_x.includes?(ix) && range_y.includes?(iy)
+        direction_scenic += 1
+        if trees[iy][ix] >= trees[y][x]
+          direction_visible = false
+          break
+        end
+        ix += mx; iy += my
+      end
+      scenic *= direction_scenic
+      is_visible ||= direction_visible
     end
+    visible += 1 if is_visible
+    max_scenic = [max_scenic, scenic].max
   end
 end
 
@@ -31,4 +33,4 @@ puts "Part 1:"
 puts visible + trees[0].size * 2 + (trees.size - 2) * 2 # all trees at the edge are visible
 
 puts "Part 2:"
-puts "Max score #{max_score} at #{best_x},#{best_y}"
+puts max_scenic
