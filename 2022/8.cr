0e7ff4d6dfac58fa2373b89e9e4b39776219d77d
Fraser McCrossan <fraser@mccrossan.ca>
2022-12-08 22:05:34 -0500

    rename scenic to scene

--- 2022/8.cr
+++ 2022/8.cr
@@ -2,12 +2,12 @@ trees = STDIN.each_line(chomp: true).map { |line| line.each_char.map { |ch| ch.o
 
 raise "bad grid" if trees.size == 0 || trees[1..].any? { |row| row.size != trees[0].size }
 
-visible, max_scenic_score = 0, 0
+visible, max_scene_score = 0, 0
 range_x, range_y = 0..(trees[0].size-1), 0..(trees.size-1)
 # we don't need to check the edge trees - at least one direction will be zero so the product will be zero
 (1...range_y.end).each do |y|
   (1...range_x.end).each do |x|
-    is_visible, scenic_score = false, 1
+    is_visible, scene_score = false, 1
     [ {0,1}, {1,0}, {0,-1}, {-1,0} ].each do |mx, my|
       view_distance = 0
       ix, iy = x+mx, y+my
@@ -16,11 +16,11 @@ range_x, range_y = 0..(trees[0].size-1), 0..(trees.size-1)
         break if trees[iy][ix] >= trees[y][x] # view out or in is blocked
         ix += mx; iy += my
       end
-      scenic_score *= view_distance
+      scene_score *= view_distance
       is_visible ||= !range_x.includes?(ix) || !range_y.includes?(iy) # if it fell off the end, must be visible
     end
     visible += is_visible ? 1 : 0
-    max_scenic_score = [max_scenic_score, scenic_score].max
+    max_scene_score = [max_scene_score, scene_score].max
   end
 end
 
@@ -28,4 +28,4 @@ puts "Part 1:"
 puts visible + trees[0].size * 2 + (trees.size - 2) * 2 # all trees at the edge are visible
 
 puts "Part 2:"
-puts max_scenic_score
+puts max_scene_score
