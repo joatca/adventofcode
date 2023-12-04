8c7f666f0cd8f7983f733f1d3edc6179e0eec0a7
Fraser McCrossan <fm@sharcnet.ca>
2023-12-04 14:16:44 -0500

    minor code cleanups and shortening, part1 this time

--- 2023/4/part1.cr
+++ 2023/4/part1.cr
@@ -1,11 +1,8 @@
 require "set"
 
 puts ARGF.each_line(chomp: true).map { |line|
-  if line =~ /^Card\s+(\d+):(.+)\|(.+)$/
-    num, winning, have = $1, $2.split.to_set, $3.split
-    1 << (have.count { |num| winning.includes?(num) } - 1)
-  else
-    raise "invalid input line: #{line}"
-  end
+  raise "invalid input line: #{line}" unless line =~ /^Card\s+\d+:(.+)\|(.+)$/
+  winning, have = $1.split.to_set, $2.split
+  1 << (have.count { |num| winning.includes?(num) } - 1)
 }.sum
 
