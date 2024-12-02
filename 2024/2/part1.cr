bc326daf8524dbe1af1648d179712e46b062ed53
Fraser <fm@sharcnet.ca>
2024-12-02 13:54:17 -0500

    remove unnecessary to_a calls

--- 2024/2/part1.cr
+++ 2024/2/part1.cr
@@ -1,4 +1,4 @@
 puts STDIN.each_line(chomp: true).map { |line| line.split.map(&.to_i) }.count { |report|
   first_diff = (report[0] <=> report[1])
-  report.each_cons(2).to_a.all? { |cons| (cons[0] <=> cons[1]) == first_diff && (1..3).includes?((cons[0]-cons[1]).abs) }
+  report.each_cons(2).all? { |cons| (cons[0] <=> cons[1]) == first_diff && (1..3).includes?((cons[0]-cons[1]).abs) }
 }
