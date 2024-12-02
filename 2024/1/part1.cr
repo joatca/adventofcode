727e464859bd68cd88eb3eb210e145235ad2206e
Fraser <fm@sharcnet.ca>
2024-12-02 13:41:49 -0500

    we don't need += in reduce, you idiot

--- 2024/1/part1.cr
+++ 2024/1/part1.cr
@@ -1,3 +1,3 @@
-puts STDIN.each_line.map { |line| line.split.map(&.to_i) }.to_a.transpose.map(&.sort!).transpose
+puts STDIN.each_line.map { |line| line.split.map(&.to_i) }.to_a
+      .transpose.map(&.sort!).transpose
       .reduce(0) { |sum, pair| sum += (pair[0]-pair[1]).abs }
-
--- 2024/1/part2.cr
+++ 2024/1/part2.cr
@@ -1,4 +1,3 @@
 left, right = STDIN.each_line.map { |line| line.split.map(&.to_i) }.to_a.transpose
 right_count = Hash(Int32, Int32).new(0).merge!(right.tally)
-puts left.reduce(0) { |total, lnum| total += lnum * right_count[lnum] }
-
+puts left.reduce(0) { |total, lnum| total + lnum * right_count[lnum] }
