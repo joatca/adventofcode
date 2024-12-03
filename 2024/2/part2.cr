7b5a94f2607e05869924584ada609f7262e2f9f1
Fraser <git@mail.mccrossan.ca>
2024-12-03 07:03:34 -0500

    shorten loop variable

--- 2024/2/part2.cr
+++ 2024/2/part2.cr
@@ -1,6 +1,6 @@
 def is_safe(report : Array(Int32))
   first_diff = (report[0] <=> report[1])
-  report.each_cons(2).all? { |cons| (cons[0] <=> cons[1]) == first_diff && (1..3).includes?((cons[0]-cons[1]).abs) }
+  report.each_cons(2).all? { |c| (c[0] <=> c[1]) == first_diff && (1..3).includes?((c[0]-c[1]).abs) }
 end
 
 puts STDIN.each_line(chomp: true).map { |line| line.split.map(&.to_i) }.count { |report|
