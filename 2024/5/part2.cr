004c836de16349bfd2c5f6a0344741a1896e6dc0
Fraser <git@mail.mccrossan.ca>
2024-12-06 21:07:44 -0500

    tidy up part2

--- 2024/5/part2.cr
+++ 2024/5/part2.cr
@@ -3,39 +3,38 @@ updates = Array(Array(Int32)).new
 
 STDIN.each_line(chomp: true) do |line|
   case line
-  when /^\s*$/
+  when /^\s*$/ # ignore blanks
     next
-  when /^(\d+)\|(\d+)$/
+  when /^(\d+)\|(\d+)$/ # before|after
     befores[$2.to_i] << $1.to_i
-  when /^[\d,]+/
+  when /^[\d,]+/ # n1,n2,n3,...
     updates << line.split(',').map { |s| s.to_i }
   end
 end
 
-correct_tot, incorrect_tot = 0, 0
+total_correct, total_incorrect = 0, 0
 updates.each do |update|
   correct = true
-  update.size.times.each do |i|
-    j = i + 1
+  update.size.times.each do |i| # i → index of current update page
+    j = i + 1 # start the loop one along from the current i
     loop do
       # find the first incorrect item
       fi = (j...update.size).find { |k| befores[update[i]].includes?(update[k]) }
-      if fi.nil?
+      if fi.nil? # if none then loop
         j += 1
         break if j >= update.size
-      else
+      else # if found, mark update as incorrect then move the incorrect item just before i
         correct = false
         # relocate the item
-        item = update.delete_at(fi)
-        update.insert(i, item)
+        update.insert(i, update.delete_at(fi))
       end
     end
   end
   if correct
-    correct_tot += update[update.size // 2]
+    total_correct += update[update.size // 2]
   else
-    incorrect_tot += update[update.size // 2]
+    total_incorrect += update[update.size // 2]
   end
 end
-puts "Correct #{correct_tot} incorrect #{incorrect_tot}"
+puts "Correct #{total_correct} incorrect #{total_incorrect}"
 
