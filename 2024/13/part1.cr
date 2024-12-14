eaaaec989cb5dd44639d0896b70c12d93cb30656
Fraser <git@mail.mccrossan.ca>
2024-12-13 21:33:56 -0500

    fixed variable names, rearranged to solve first for B instead of A, still doen't work for part2

--- 2024/13/part1.cr
+++ 2024/13/part1.cr
@@ -1,22 +1,27 @@
 A_COST = 3_i64
 B_COST = 1_i64
-PRIZE_OFFSET = 10000000000000_i64
+PRIZE_OFFSET = 0_i64 #10000000000000_i64
 
-def least_tokens(ca1 : Int64, ca2 : Int64, cb1 : Int64, cb2 : Int64, va : Int64, vb : Int64)
-  # else try to solve this as a pair of simultaneous equations ca1×a + ca2×b = va and cb1×a + cb2×b == vb
-  # multiply each equation by the opposite's b coefficient
-  ca1, cb1, va, ca2, cb2, vb = ca1 * cb2, cb1 * cb2, va * cb2, ca2 * cb1, cb2 * cb1, vb * cb1
-  # subtract one from the other; y coefficient will be zero so we only need to calculate x coefficient and the result
-  c = ca1 - ca2
-  v = va - vb
-  a, amod = v.divmod(c)
-  if amod != 0_i64 # one solution but not integral, game isn't winnable, spend no tokens
+def least_tokens(buta_x : Int64, buta_y : Int64, butb_x : Int64, butb_y : Int64, prize_x : Int64, prize_y : Int64)
+  # else try to solve this as a pair of simultaneous equations buta_x×a + butb_x×b = prize_x and buta_y×a + butb_y×b == prize_y
+  # multiply each equation by the opposite's a coefficient
+  buta_x, butb_x, prize_x, buta_y, butb_y, prize_y = buta_x * buta_y, butb_x * buta_y, prize_x * buta_y, buta_y * buta_x, butb_y * buta_x, prize_y * buta_x
+  # subtract one from the other; a coefficient will be zero so we only need to calculate b coefficient and the result
+  c = butb_x - butb_y
+  cdebug = buta_x - buta_y
+  raise "oops" unless cdebug == 0
+  v = prize_x - prize_y
+  b, bmod = v.divmod(c)
+  if bmod != 0_i64 # one solution but not integral, game isn't winnable, spend no tokens
     puts "not winnable"
+    0_i64
+  elsif b == 0
+    raise "oops"
     0_i64
   else
     # exactly one solution, calculate the number of tokens; this will be after rearranging
-    #  ca1×a + cb1×b = va to b = (va - ca1×a)/cb1
-    A_COST * a + B_COST * ((va - ca1 * a) // cb1)
+    #  buta_x×a + butb_x×b = prize_x to a = (prize_x - butb_x×b)/buta_x
+    B_COST * b + A_COST * ((prize_x - butb_x * b) // buta_x)
   end
 end
 
@@ -38,10 +43,6 @@ STDIN.each_line(chomp: true) do |line|
     puts line
     prizex, prizey = $1.to_i64 + PRIZE_OFFSET, $2.to_i64 + PRIZE_OFFSET
     puts "a (#{ax},#{ay}) b (#{bx},#{by}) prize (#{prizex},#{prizey})"
-    # if prizex % bx == 0 && prizey % by == 0 && prizex // bx == prizey // by
-    #   sleep 3
-    #   raise "exact B"
-    # end
     tokens += least_tokens(ax, ay, bx, by, prizex, prizey)
   end
 end
