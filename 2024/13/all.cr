ac4f6eaf1dc82a224ef21fc2ffa6a067db4456dc
Fraser <git@mail.mccrossan.ca>
2024-12-13 22:02:28 -0500

    works with Cramer's rule - where the fuck is the error in my code? It's doing the same thing in a cruder way, why the fuck doesn't it work?

--- 2024/13/all.cr
+++ 2024/13/all.cr
@@ -1,27 +1,16 @@
 A_COST = 3_i64
 B_COST = 1_i64
-PRIZE_OFFSET = 0_i64 #10000000000000_i64
+#PRIZE_OFFSET = 0_i64
+PRIZE_OFFSET = 10000000000000_i64
 
 def least_tokens(buta_x : Int64, buta_y : Int64, butb_x : Int64, butb_y : Int64, prize_x : Int64, prize_y : Int64)
-  # else try to solve this as a pair of simultaneous equations buta_x×a + butb_x×b = prize_x and buta_y×a + butb_y×b == prize_y
-  # multiply each equation by the opposite's a coefficient
-  buta_x, butb_x, prize_x, buta_y, butb_y, prize_y = buta_x * buta_y, butb_x * buta_y, prize_x * buta_y, buta_y * buta_x, butb_y * buta_x, prize_y * buta_x
-  # subtract one from the other; a coefficient will be zero so we only need to calculate b coefficient and the result
-  c = butb_x - butb_y
-  cdebug = buta_x - buta_y
-  raise "oops" unless cdebug == 0
-  v = prize_x - prize_y
-  b, bmod = v.divmod(c)
-  if bmod != 0_i64 # one solution but not integral, game isn't winnable, spend no tokens
-    puts "not winnable"
-    0_i64
-  elsif b == 0
-    raise "oops"
-    0_i64
+  determinant = buta_x*butb_y - buta_y*butb_x
+  a = (prize_x * butb_y - prize_y * butb_x) // determinant
+  b = (buta_x * prize_y - buta_y * prize_x) // determinant
+  if a * buta_x + b * butb_x == prize_x && a * buta_y + b * butb_y == prize_y
+    return a * A_COST + b * B_COST
   else
-    # exactly one solution, calculate the number of tokens; this will be after rearranging
-    #  buta_x×a + butb_x×b = prize_x to a = (prize_x - butb_x×b)/buta_x
-    B_COST * b + A_COST * ((prize_x - butb_x * b) // buta_x)
+    return 0_i64
   end
 end
 
